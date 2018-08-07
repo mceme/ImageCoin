8c862df74347cd66808a793f1bd152f02eebe828
mceme <manuelcostapnf@hotmail.com>
2018-08-07 01:39:48 +0100

    subframe

--- src/qt/walletframe.h
+++ src/qt/walletframe.h
@@ -66,6 +66,9 @@ public Q_SLOTS:
     /** Switch to send coins page */
     void gotoSendCoinsPage(QString addr = "");
 
+    /** Switch to send coins page */
+       void gotoEncrpytDecryptPage();
+
     /** Show Sign/Verify Message dialog and switch to sign message tab */
     void gotoSignMessageTab(QString addr = "");
     /** Show Sign/Verify Message dialog and switch to verify message tab */
