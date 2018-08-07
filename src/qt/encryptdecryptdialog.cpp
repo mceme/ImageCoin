31e86b5fd52955a0604b005c3b98a7dac9ed9e39
mceme <manuelcostapnf@hotmail.com>
2018-08-07 04:51:09 +0100

    sub

--- src/qt/encryptdecryptdialog.cpp
+++ src/qt/encryptdecryptdialog.cpp
@@ -16,7 +16,7 @@
 
 
 
-EncryptDecryptDialog::EncryptDecryptDialog(const PlatformStyle *platformStyle, QWidget *parent) :
+EncryptDecryptDialog::EncryptDecryptDialog(const PlatformStyle *platformStyle, QWidget *parent = 0) :
     QDialog(parent),
     ui(new Ui::EncryptDecryptDialog),
 	clientModel(0),
