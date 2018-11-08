e0b6dfca0156b9f8beb987ca7029d52f4e0f6e88
mceme <manuelcostapnf@hotmail.com>
2018-11-08 22:16:27 +0000

    sub

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -5,7 +5,7 @@
 
 #ifndef BITCOIN_PRIMITIVES_TRANSACTION_H
 #define BITCOIN_PRIMITIVES_TRANSACTION_H
-#include "policy/policy.h"
+
 #include "amount.h"
 #include "script/script.h"
 #include "serialize.h"
