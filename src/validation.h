95f3d43bcc704024e72fa70530e84346a8a1cd69
mceme <manuelcostapnf@hotmail.com>
2019-04-10 22:18:09 +0100

    sub

--- src/validation.h
+++ src/validation.h
@@ -104,10 +104,10 @@ static const unsigned int AVG_ADDRESS_BROADCAST_INTERVAL = 30;
 /** Average delay between trickled inventory broadcasts in seconds.
  *  Blocks, whitelisted receivers, and a random 25% of transactions bypass this. */
 static const unsigned int AVG_INVENTORY_BROADCAST_INTERVAL = 5;
-/** Block download timeout base, expressed in millionths of the block interval (i.e. 2.5 min) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 250000;
-/** Additional block download timeout per parallel downloading peer (i.e. 1.25 min) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 125000;
+/** Block download timeout base, expressed in millionths of the block interval (i.e. 6.5 min) */
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 650000;
+/** Additional block download timeout per parallel downloading peer (i.e. 6.25 min) */
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 625000;
 
 static const unsigned int DEFAULT_LIMITFREERELAY = 15;
 static const bool DEFAULT_RELAYPRIORITY = true;
