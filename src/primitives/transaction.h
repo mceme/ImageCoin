3111882a55c51eb72b620df2b4f39bfda161b4dd
mceme <manuelcostapnf@hotmail.com>
2019-05-30 23:26:21 +0100

    Update transaction.h

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2018- The ImageCoin Core developer mceme
+// Copyright (c) 2018- The ImageCoin Core mceme
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
