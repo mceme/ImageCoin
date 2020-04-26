8553a0c859e9716e75b350a226a04cba7377714b
mceme <manuelcostapnf@hotmail.com>
2020-04-26 07:05:15 +0100

    Update img-install.sh

--- imginstall/img-install.sh
+++ imginstall/img-install.sh
@@ -6,7 +6,7 @@ COIN_DAEMON='/usr/local/bin/ImageCoind'
 COIN_DAEMON_NAME='ImageCoind'
 COIN_CLI_NAME='ImageCoin-cli'
 COIN_CLI='/usr/local/bin/ImageCoin-cli'
-COIN_REPO='https://github.com/mceme/ImageCoin/releases/download/1.0.33/ImageCoin_linux_32_64.tar.xz'
+COIN_REPO='https://github.com/mceme/ImageCoin/releases/download/1.0.35/ImageCoin_linux_x86_64.tar.xz'
 COIN_NAME='ImageCoin'
 COIN_PORT=6998
 
