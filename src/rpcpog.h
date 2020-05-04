34c3c83c2d797b93fe6572abe42aa23a385086df
mceme <manuelcostapnf@hotmail.com>
2020-05-04 12:25:16 +0100

    Update rpcpog.h

--- src/rpcpog.h
+++ src/rpcpog.h
@@ -150,20 +150,20 @@ struct DACProposal
 	std::string sProposalHRTime;
 };
 
-/** Comparison function for sorting the getchaintips heads.  */
-struct CompareBlocksByHeight
-{
-    bool operator()(const CBlockIndex* a, const CBlockIndex* b) const
-    {
-        /* Make sure that unequal blocks with the same height do not compare
-           equal. Use the pointers themselves to make a distinction. */
-
-        if (a->nHeight != b->nHeight)
-          return (a->nHeight > b->nHeight);
-
-        return a < b;
-    }
-};
+///** Comparison function for sorting the getchaintips heads.  */
+//struct CompareBlocksByHeight
+//{
+//    bool operator()(const CBlockIndex* a, const CBlockIndex* b) const
+//    {
+//        /* Make sure that unequal blocks with the same height do not compare
+//           equal. Use the pointers themselves to make a distinction. */
+//
+//        if (a->nHeight != b->nHeight)
+//          return (a->nHeight > b->nHeight);
+//
+//        return a < b;
+//    }
+//};
 
 CAmount CAmountFromValue(const UniValue& value);
 std::string RoundToString(double d, int place);
