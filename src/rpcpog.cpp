a428968ad1af4f5f8d0a85b0a647a2cac1a145d2
mceme <manuelcostapnf@hotmail.com>
2020-05-04 02:59:54 +0100

    sub

--- src/rpcpog.cpp
+++ src/rpcpog.cpp
@@ -42,7 +42,7 @@
 #include "netbase.h" // for LookupHost
 #include "wallet/wallet.h"
 #include <sstream>
-#include "randomx_bbp.h"
+//#include "randomx_bbp.h"
 
 #ifdef ENABLE_WALLET
 extern CWallet* pwalletMain;
