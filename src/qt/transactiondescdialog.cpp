9f0cb7393264d76df0751e168f972e1f5502776c
mceme <manuelcostapnf@hotmail.com>
2020-10-31 06:38:14 +0000

    Update transactiondescdialog.cpp

--- src/qt/transactiondescdialog.cpp
+++ src/qt/transactiondescdialog.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "guiutil.h"
+#include "util.h"
 #include "transactiondescdialog.h"
 #include "ui_transactiondescdialog.h"
 
